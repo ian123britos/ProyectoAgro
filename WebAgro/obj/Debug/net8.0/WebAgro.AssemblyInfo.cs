4fdc8a4547e3469abad0ec23328aa4ab7eb4b068
Ian Britos Borgunder <IB319561@fi365.ort.edu.uy>
2025-08-20 20:55:59 -0300

    algo

--- WebAgro/obj/Debug/net8.0/WebAgro.AssemblyInfo.cs
+++ WebAgro/obj/Debug/net8.0/WebAgro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAgro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+024de26012452f27d494ac631ea39fb791553070")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8185bdb4258290b9fadbb7704aac8e90057df365")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAgro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAgro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
